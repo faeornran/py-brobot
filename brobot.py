7863f85242c5b07c9e4c474938c444fae2d1e770
James Vaughan <jvaughan@sfo-jvaughan-C02HW1BYF1G3.local>
2012-07-05 17:36:33 -0700

    dnkgajgdsnakagsdjnlads

---
+++ brobot.py
@@ -0,0 +1,19 @@
+NICKNAME = "Brobot"
+PORT = 6667
+SERVER = "irc.saymedia.com"
+CHANNELS = ["#chatter"]
+
+class Brobot
+	def __init__(self):
+		this.connected = false
+		this.nick = nickname
+		this.port = port
+		this.server = server
+		this.channels = channels
+		connect()
+
+
+	def connect(self):
+		
+		
+
